5714bf9bf57166a151ce0c4cdce7b374df688bb5
sjpascua <sheryljoy@armyspy.com>
2016-03-10 07:47:41 +0800

    changed function name from MyFunc to Funcout

--- function_stdoutput.py
+++ function_stdoutput.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #Author: Sheryl Joy S. Pascua
 #Date: March 9, 2016
-#Purpose: Python Script for displaying ouput in three ways
+#Purpose: Python Script for displaying output in three ways
 
 def FuncOut(name, age):
           print "Hi! My name is ", name + "and my age is", age
